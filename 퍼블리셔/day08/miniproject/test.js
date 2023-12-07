aeda8fef6b570e79b50018957f5dc7259769a13b
uoohs2 <97604227+uoohs2@users.noreply.github.com>
2023-12-07 18:02:35 +0900

    Add files via upload

---
+++ 퍼블리셔/day08/miniproject/test.js
@@ -0,0 +1,328 @@
+const a = [
+  {
+    id: '25tgd2oCHW',
+    name: '김기범',
+    position: '선생님',
+    gymName: '공덕 핏인라이프',
+    gymAddress: '서울 마포구 대흥동',
+    profileImage:
+      'https://api.da-gym.net/trainers/images/25tgd2oCHW+42HbqetNiCy.jpg',
+    exerciseTypes: ['P.T'],
+    isFreeTrial: false,
+    isOnetimeLesson: false,
+    paymentAvailable: true,
+    rate: 0,
+    reviewCount: 0,
+  },
+  {
+    id: '23XF76BeJF',
+    name: '이광호',
+    position: '선생님',
+    gymName: '공덕 핏인라이프',
+    gymAddress: '서울 마포구 대흥동',
+    profileImage:
+      'https://api.da-gym.net/trainers/images/23XF76BeJF+2NpPbyrCcgchVk4ojQmu4.jpg',
+    exerciseTypes: ['P.T'],
+    isFreeTrial: false,
+    isOnetimeLesson: true,
+    paymentAvailable: true,
+    rate: 0,
+    reviewCount: 0,
+  },
+  {
+    id: '26N5CMSr4p',
+    name: '김주아',
+    position: '선생님',
+    gymName: '대흥 필라테스드주아',
+    gymAddress: '서울 마포구 대흥동',
+    profileImage:
+      'https://api.da-gym.net/trainers/images/26N5CMSr4p+2NqVjofzAvjHSbCMXpAMZ.jpg',
+    exerciseTypes: ['P.T'],
+    isFreeTrial: false,
+    isOnetimeLesson: true,
+    paymentAvailable: true,
+    rate: 0,
+    reviewCount: 0,
+  },
+  {
+    id: '6LEehsN4hr',
+    name: '최종윤',
+    position: '선생님',
+    gymName: '공덕 머슬마인드&필라마인드',
+    gymAddress: '서울 마포구 도화동',
+    profileImage:
+      'https://api.da-gym.net/trainers/images/6LEehsN4hr+DU6QibEvrxYu.jpg',
+    exerciseTypes: ['P.T'],
+    isFreeTrial: false,
+    isOnetimeLesson: true,
+    paymentAvailable: true,
+    rate: 0,
+    reviewCount: 0,
+  },
+  {
+    id: '2Jr7xDjr2o',
+    name: '김유찬',
+    position: '선생님',
+    gymName: '공덕 머슬마인드&필라마인드',
+    gymAddress: '서울 마포구 도화동',
+    profileImage:
+      'https://api.da-gym.net/trainers/images/2Jr7xDjr2o+Rfci3dA7R1do.jpg',
+    exerciseTypes: ['P.T'],
+    isFreeTrial: false,
+    isOnetimeLesson: true,
+    paymentAvailable: true,
+    rate: 0,
+    reviewCount: 0,
+  },
+  {
+    id: '2AR7YKK1ee',
+    name: '김진태',
+    position: '선생님',
+    gymName: '공덕 머슬마인드&필라마인드',
+    gymAddress: '서울 마포구 도화동',
+    profileImage:
+      'https://api.da-gym.net/trainers/images/2AR7YKK1ee+TD6t9zkcK2TH.jpg',
+    exerciseTypes: ['P.T'],
+    isFreeTrial: false,
+    isOnetimeLesson: true,
+    paymentAvailable: true,
+    rate: 0,
+    reviewCount: 0,
+  },
+  {
+    id: '8RG3tdVAGj',
+    name: '이수창',
+    position: '선생님',
+    gymName: '공덕 머슬마인드&필라마인드',
+    gymAddress: '서울 마포구 도화동',
+    profileImage:
+      'https://api.da-gym.net/trainers/images/8RG3tdVAGj+RvAiomJEchoh.jpg',
+    exerciseTypes: ['P.T'],
+    isFreeTrial: false,
+    isOnetimeLesson: false,
+    paymentAvailable: true,
+    rate: 0,
+    reviewCount: 0,
+  },
+  {
+    id: '2KWxnVnzzL',
+    name: '서범식',
+    position: '선생님',
+    gymName: '공덕 머슬마인드&필라마인드',
+    gymAddress: '서울 마포구 도화동',
+    profileImage:
+      'https://api.da-gym.net/trainers/images/2KWxnVnzzL+bYNAA4UmNtV.jpg',
+    exerciseTypes: ['P.T'],
+    isFreeTrial: false,
+    isOnetimeLesson: false,
+    paymentAvailable: true,
+    rate: 0,
+    reviewCount: 0,
+  },
+  {
+    id: '2SWK4RKcvX',
+    name: '이원찬',
+    position: '선생님',
+    gymName: '공덕 리본필라테스',
+    gymAddress: '서울 마포구 도화동',
+    profileImage:
+      'https://api.da-gym.net/trainers/images/2SWK4RKcvX+3kfmUAuNXjMxMmqTHpMkK.jpg',
+    exerciseTypes: ['P.T'],
+    isFreeTrial: false,
+    isOnetimeLesson: true,
+    paymentAvailable: true,
+    rate: 0,
+    reviewCount: 0,
+  },
+  {
+    id: '3Ar26Meg3S',
+    name: '유지선',
+    position: '선생님',
+    gymName: '공덕 리본필라테스',
+    gymAddress: '서울 마포구 도화동',
+    profileImage:
+      'https://api.da-gym.net/trainers/images/3Ar26Meg3S+cjezh2w7Qu24vxN6NER.jpg',
+    exerciseTypes: ['P.T'],
+    isFreeTrial: false,
+    isOnetimeLesson: true,
+    paymentAvailable: true,
+    rate: 0,
+    reviewCount: 0,
+  },
+  {
+    id: 'idvSJTpS4',
+    name: '조윤화',
+    position: '선생님',
+    gymName: '공덕 리본필라테스',
+    gymAddress: '서울 마포구 도화동',
+    profileImage:
+      'https://api.da-gym.net/trainers/images/idvSJTpS4+2QTu7XnsHQYPzL3aNDSXR.jpg',
+    exerciseTypes: ['P.T'],
+    isFreeTrial: false,
+    isOnetimeLesson: true,
+    paymentAvailable: true,
+    rate: 0,
+    reviewCount: 0,
+  },
+  {
+    id: 'toX62AhX1',
+    name: '전우리',
+    position: '선생님',
+    gymName: '공덕 리본필라테스',
+    gymAddress: '서울 마포구 도화동',
+    profileImage:
+      'https://api.da-gym.net/trainers/images/toX62AhX1+2SPjjKqnpV77j8fKVhB3P.jpg',
+    exerciseTypes: ['P.T'],
+    isFreeTrial: false,
+    isOnetimeLesson: true,
+    paymentAvailable: true,
+    rate: 0,
+    reviewCount: 0,
+  },
+  {
+    id: '5Y5aSGFvQL',
+    name: '이동우',
+    position: '선생님',
+    gymName: '광흥창 유디티짐',
+    gymAddress: '서울 마포구 신수동',
+    profileImage:
+      'https://api.da-gym.net/trainers/images/5Y5aSGFvQL+2bq4ri52yk5X74fpQCodH.jpg',
+    exerciseTypes: ['P.T'],
+    isFreeTrial: false,
+    isOnetimeLesson: true,
+    paymentAvailable: true,
+    rate: 0,
+    reviewCount: 0,
+  },
+  {
+    id: '2M85s4DrPJ',
+    name: '김상열',
+    position: '선생님',
+    gymName: '밤섬 피트니스랩',
+    gymAddress: '서울 마포구 하중동',
+    profileImage:
+      'https://api.da-gym.net/trainers/images/2M85s4DrPJ+2QkpHpdCXtZ9DmiAhtyuw.jpg',
+    exerciseTypes: ['P.T'],
+    isFreeTrial: false,
+    isOnetimeLesson: true,
+    paymentAvailable: true,
+    rate: 0,
+    reviewCount: 0,
+  },
+  {
+    id: '2BPPL2okE9',
+    name: '안세영',
+    position: '선생님',
+    gymName: '밤섬 피트니스랩',
+    gymAddress: '서울 마포구 하중동',
+    profileImage:
+      'https://api.da-gym.net/trainers/images/2BPPL2okE9+2P9WUHt52hHea3bHUaKC9.jpg',
+    exerciseTypes: ['P.T'],
+    isFreeTrial: false,
+    isOnetimeLesson: true,
+    paymentAvailable: true,
+    rate: 0,
+    reviewCount: 0,
+  },
+  {
+    id: '2MaUdHsgQP',
+    name: '주현욱',
+    position: '선생님',
+    gymName: '밤섬 피트니스랩',
+    gymAddress: '서울 마포구 하중동',
+    profileImage:
+      'https://api.da-gym.net/trainers/images/2MaUdHsgQP+eDHCgo8QhT6UKRFnMWQ.jpg',
+    exerciseTypes: ['P.T'],
+    isFreeTrial: false,
+    isOnetimeLesson: false,
+    paymentAvailable: true,
+    rate: 0,
+    reviewCount: 0,
+  },
+  {
+    id: '3P58tU5we6',
+    name: '김석',
+    position: '선생님',
+    gymName: '공덕 킬러바디짐',
+    gymAddress: '서울 마포구 공덕동',
+    profileImage:
+      'https://api.da-gym.net/trainers/images/3P58tU5we6+2QTuwep8FHDxYLk2b1XJZ.jpg',
+    exerciseTypes: ['P.T'],
+    isFreeTrial: false,
+    isOnetimeLesson: true,
+    paymentAvailable: true,
+    rate: 0,
+    reviewCount: 0,
+  },
+  {
+    id: 'eNDYGLzRz',
+    name: '김진혁',
+    position: '선생님',
+    gymName: '신촌 비나이더피트니스',
+    gymAddress: '서울 마포구 노고산동',
+    profileImage:
+      'https://api.da-gym.net/trainers/images/eNDYGLzRz+2NrXTbNJyjcYbSi7DZVY1.jpg',
+    exerciseTypes: ['P.T'],
+    isFreeTrial: false,
+    isOnetimeLesson: true,
+    paymentAvailable: true,
+    rate: 0,
+    reviewCount: 0,
+  },
+  {
+    id: '25MK47HBpC',
+    name: '김민기',
+    position: '선생님',
+    gymName: '신촌 비나이더피트니스',
+    gymAddress: '서울 마포구 노고산동',
+    profileImage:
+      'https://api.da-gym.net/trainers/images/25MK47HBpC+2NrE8kuLPtj7QutfgxiJG.jpg',
+    exerciseTypes: ['P.T'],
+    isFreeTrial: false,
+    isOnetimeLesson: false,
+    paymentAvailable: true,
+    rate: 0,
+    reviewCount: 0,
+  },
+  {
+    id: 'kufsC2xyG',
+    name: '오채윤',
+    position: '선생님',
+    gymName: '신촌 비나이더피트니스',
+    gymAddress: '서울 마포구 노고산동',
+    profileImage:
+      'https://api.da-gym.net/trainers/images/kufsC2xyG+2S5fFSse1JHVgV4FLKwzs.jpg',
+    exerciseTypes: ['P.T'],
+    isFreeTrial: false,
+    isOnetimeLesson: true,
+    paymentAvailable: true,
+    rate: 0,
+    reviewCount: 0,
+  },
+];
+
+const modifiedArray = a.map(({ name, gymName, gymAddress, profileImage }) => ({
+  name,
+  gymName,
+  gymAddress,
+  profileImage,
+}));
+
+const trainers = [...modifiedArray, ...modifiedArray, ...modifiedArray];
+// .container 요소 선택
+const container = document.querySelector('.cardBox');
+
+// 각 트레이너의 데이터를 사용하여 mainBox 생성 및 추가
+trainers.forEach((trainer) => {
+  const mainBox = document.createElement('div');
+  mainBox.classList.add('card');
+  mainBox.innerHTML = `
+          <img src="${trainer.profileImage}" alt="gym" />
+          <div class="cardInfo">
+            <h3 class="name">${trainer.name}</h3>
+            <p class="place">${trainer.gymName}</p>
+            <button class="type">P.T</button>
+          </div>
+    `;
+  container.appendChild(mainBox);
+});
